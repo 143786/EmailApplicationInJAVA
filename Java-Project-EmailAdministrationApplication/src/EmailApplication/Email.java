91fbbbb4e5f2c70fc34c9e082942b01876a4b348
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-08 00:39:44 +0200

    Add files via upload

--- Java-Project-EmailAdministrationApplication/src/EmailApplication/Email.java
+++ Java-Project-EmailAdministrationApplication/src/EmailApplication/Email.java
@@ -18,6 +18,7 @@ public class Email {
       // Call a method asking for the department - return the department.
       this.department = setDepartment();
       System.out.println("Department: " + this.department);
+      // Call a method that return a random password :
    }
     // ask for the department.
    private String setDepartment(){
@@ -29,14 +30,19 @@ public class Email {
       else if (depChoice == 3) { return "acct"; }
       else { return ""; }
 
-
       }
 
-
-
-
-
     // Generate a random password.
+   private String randomPassword(int length) {
+      String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%" ;
+      char[] password = new char[length];
+      for (int i=0; i<length; i++) {
+         int rand = (int) (Math.random() * passwordSet.length());
+         password[i] =  passwordSet.charAt(rand);
+
+      }
+      return new String(password);
+
     // Set the mailbox capacity.
     // Set the alternate email.
     // Change the password.
