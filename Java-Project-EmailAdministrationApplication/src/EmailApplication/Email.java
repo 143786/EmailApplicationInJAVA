dc15e612730fa71ac4d0bc9af856b927b5125e23
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-07 16:01:23 +0200

    Add files via upload

--- Java-Project-EmailAdministrationApplication/src/EmailApplication/Email.java
+++ Java-Project-EmailAdministrationApplication/src/EmailApplication/Email.java
@@ -16,16 +16,24 @@ public class Email {
       this.lastName = lastName;
       System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName );
       // Call a method asking for the department - return the department.
+      this.department = setDepartment();
+      System.out.println("Department: " + this.department);
    }
     // ask for the department.
    private String setDepartment(){
-      System.out.println("Enter the department\n1 for sales\n2 for Development\n3 for Acounting\n0 for none ");
+      System.out.println("DEPARTMENT CODES\n1 for sales\n2 for Development\n3 for Acounting\n0 for none\n Enter department code ");
       Scanner in = new Scanner(System.in);
       int depChoice = in.nextInt();
       if (depChoice == 1) {return "sales"; }
-      else if (depChoice == 2) { return "dev";}
+      else if (depChoice == 2) { return "dev"; }
+      else if (depChoice == 3) { return "acct"; }
+      else { return ""; }
+
+
       }
-   }
+
+
+
 
 
     // Generate a random password.
