f77970ab6bab769f75dee7447268fc8f437e1868
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-08 11:33:44 +0200

    Add files via upload

--- Java-Project-EmailAdministrationApplication/src/EmailApplication/EmailApp.java
+++ Java-Project-EmailAdministrationApplication/src/EmailApplication/EmailApp.java
@@ -3,6 +3,8 @@ package EmailApplication;
 public class EmailApp {
     public static void main(String[] args) {
         Email eml = new Email ("Ayline", "Tabish");
+        eml.setAlternateEmail("at@gmail.com");
+        System.out.println(eml.getAlternateEmail());
 
     }
 }
